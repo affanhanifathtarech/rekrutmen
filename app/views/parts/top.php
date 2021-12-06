ae3a717a4d6ec16a4a39b9ca97b3a624440135fa
affanhanifathtarech <affanhanifathtarech@gmail.com>
2021-12-06 11:02:32 +0700

    Perbaikan Ikon

--- app/views/parts/top.php
+++ app/views/parts/top.php
@@ -3,7 +3,12 @@
 <head id="head">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="description" content="<?= $caption;?>"> -->
+    <meta property="og:image" itemprop="image" content="public/images/logo.webp"> 
+    <meta name="author" content="Karisma Polsri">
+    <meta name="generator" content="Karisma Polsri">   
+    <link rel="icon" type="image/png" href="public/images/logo.webp">
     <title><?= $data['title'] ?></title>
     <link href="public/assets/bootstrap/bootstrap@5.1.3.min.css" rel="stylesheet">
 </head>
