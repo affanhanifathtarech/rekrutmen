95ffed360cfc597752681103e32a26a04183b7f2
irpansyah2m1d <irpansyah810@gmail.com>
2021-12-07 10:46:39 +0700

    Menambahkan fitur multiple upload

--- app/views/dokumen/index.php
+++ app/views/dokumen/index.php
@@ -1,21 +1,35 @@
-<?php 
-$glob = glob('public/dokumen/rekomendasi/'. $_SESSION['nim'] . ".*");
-$display = (empty($glob)) ? 'style="display:none;"' : '';
-$link = (empty($glob)) ? '' : $glob[0] ;
+<?php
+// $file_rekomendasi = 
+// $file_rekomendasi = explode('/', $this->model('Dokumen_model')->getFileRekomendasi()); 
+// $file_rekomendasi = end($file_rekomendasi);
+// var_dump($file_rekomendasi);
+// die();
 ?>
 
-<div class="container p-3">
-    <h1 class="h3 mb-3">Dokumen</h1>
-    <div class="mb-5">
-        <label for="file_rekomendasi" class="form-label">Upload File Rekomendasi HMJ</label>
-        <input class="form-control" type="file" id="file_rekomendasi">
-        <div class="d-grid gap-2 d-md-block mt-2">
-            <a href="<?= $link;?>" target='_BLANK' class="btn btn-info rekomendasi" <?= $display ?>>Lihat Dokumen</a>
-        </div>
+<div class="row">
+    <div class="col-lg-6">
+        <h3>Upload File Rekomendasi HMJ</h3>
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="file" name="file_rekomendasi" id="file_rekomendasi">
     </div>
-    <div class="mb-3">
-        <label for="file_serfikat" class="form-label">Upload File Sertifikat Pendukung ( Optional )</label>
-        <input class="form-control" type="file" name="file_sertifikat[]" id="file_sertifikat" multiple>
+    <div class="col-lg-6">
+        <a href="<?= $this->model('Dokumen_model')->getFileRekomendasi(); ?>"><?= $_SESSION["nim"] . '.pdf'; ?></a>
+    </div>
+</div>
+<br><br><br>
+<div class="row">
+    <div class="col-lg-6">
+        <h3>Upload File Sertifikat Pendukung ( Optional )</h3>
+            <input type="file" name="file_sertifikat" id="file_sertifikat" multiple>
+        </form>
+    </div>
+
+    <div class="col-lg-6">
+        <ul>
+            <li>File Sertifikat 1</li>
+            <li>File Sertifikat 2</li>
+            <li>File Sertifikat 3</li>
+        </ul>
     </div>
 </div>
 
@@ -23,11 +37,13 @@ $link = (empty($glob)) ? '' : $glob[0] ;
 
 <script>
     $(function(){
-        $('input[type=file]').change(function(e) {
+        $('#file_rekomendasi').change(function(e) {
             e.preventDefault();
             var file_data = $("#file_rekomendasi").prop("files")[0];   
+            // var foto_lama = $("#file_rekomendasi").attr('src');
             var form_data = new FormData();
             form_data.append("file_rekomendasi", file_data);
+            // form_data.append("file_rekomendasi_lamo", foto_lama);
             $.ajax({
                 url: "dokumen/saveDokumen",
                 dataType: 'JSON',
@@ -37,11 +53,99 @@ $link = (empty($glob)) ? '' : $glob[0] ;
                 data: form_data,                      
                 type: 'post',
                 success: function(data){
-                    if(data.status==1){
-                        $('.rekomendasi').attr('href', data.data.url);
-                    }
+                    // if (data.status==1){
+                    //     $('#foto_profile').attr('src', data.data.url);
+                    // } else if (data.status==0){
+                    //     alert(data.data.msg);
+                    // }
+                    console.log(data);
                 }
              });
         });
+        $('#file_sertifikat').change(function(e) {
+            e.preventDefault();
+            var fd = new FormData();  
+
+            for (var i = 0, len = document.getElementById('file_sertifikat').files.length; i < len; i++) {
+                fd.append("file_sertifikat", document.getElementById('file_sertifikat').files[i]); 
+                    $.ajax({
+                url: 'dokumen/saveDokumenSertifikat',
+                data: fd,
+                processData: false,
+                contentType: false,
+                type: 'POST',      
+                success: function(data){
+                    console.log(data);
+                }      
+                })         
+            }
+
+            
+            // var file_data = $("#file_sertifikat").prop("files"); 
+            // var form_data = new FormData();
+            // for(var i = 0; i < file_data.length; i++){
+            //     form_data.append("file_sertifikat[]", file_data);
+            // }  
+            // // var foto_lama = $("#file_sertifikat").attr('src');
+           
+           
+            // // form_data.append("file_sertifikat_lamo", foto_lama);
+            // $.ajax({
+            //     url: "dokumen/saveDokumenSertifikat",
+            //     dataType: 'JSON',
+            //     cache: false,
+            //     contentType: false,
+            //     processData: false,
+            //     data: form_data,                      
+            //     type: 'post',
+            //     success: function(data){
+            //         // if (data.status==1){
+            //         //     $('#foto_profile').attr('src', data.data.url);
+            //         // } else if (data.status==0){
+            //         //     alert(data.data.msg);
+            //         // }
+            //         console.log(data);
+            //     }
+            //  });
+            
+        });
+    //     $('#file_sertifikat').change(function() {
+    // var filedata = $('#file_sertifikat'),
+    //         formdata = false;
+    // if (window.FormData) {
+    //     formdata = new FormData();
+    // }
+    // var i = 0, len = filedata.files.length;
+
+    // for (i; i < len; i++) {
+    //     file = filedata.files[i];
+
+    //     if (window.FileReader) {
+    //         reader = new FileReader();
+    //         reader.onloadend = function(e) {
+    //             showUploadedItem(e.target.result, file.fileName);
+    //         };
+    //         reader.readAsDataURL(file);
+    //     }
+    //     if (formdata) {
+    //         formdata.append("file_sertifikat", file);
+    //     }
+    // }
+    // if (formdata) {
+    //     $.ajax({
+    //         url: "dokumen/saveDokumenSertifikat",
+    //         type: "POST",
+    //         data: formdata,
+    //         processData: false,
+    //         contentType: false,
+    //         success: function(res) {
+    //             console.log(res);
+    //         },       
+    //         error: function(res) {
+
+    //          }       
+    //          });
+    //         }
+    //     });
     });
 </script>
